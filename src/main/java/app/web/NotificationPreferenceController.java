9659a0c2ce1d645527b9ad5296e57c2e0bdc4082
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2026-07-09 22:19:16 +0300

    Microservices exercises

--- src/main/java/app/web/NotificationPreferenceController.java
+++ src/main/java/app/web/NotificationPreferenceController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/notification-preferences")
+@RequestMapping("/api/v1/notifications/preferences")
 @RequiredArgsConstructor
 public class NotificationPreferenceController {
 
